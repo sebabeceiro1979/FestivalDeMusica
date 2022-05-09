398e2b5f883f8d27a362aef37f24543b4b6e9459
sebabeceiro_git <sebabeceiro@gmail.com>
2022-05-09 15:18:14 -0300

    first commit

---
+++ js/appData.js
@@ -0,0 +1,13 @@
+var vueApp = new Vue({
+    el: "#vueApp",
+    data: {
+        dayOne: [{
+            rockBands: ['24:00 | Metallica', '22:00 | Pearl Jam', '20:00 | KoRn', '19:00 | Muse', '18:00 | Breaking Benjamin', '17:00 | 30 Seconds to Mars'],
+            emsBands: ['24:00 | Deadmau5', '22:00 | Tïesto', '20:00 | Hardwell', '19:00 | Dash Berlin', '18:00 | Fedde Legrand', '17:00 | Audien'],
+        }],
+        dayTwo: [{
+            rockBands2: ['24:00 | Red Hot Chili Peppers', '22:00 | The Killers', '20:00 | Deftones', '19:00 | Limp Bizkit', '18:00 | Papa Roach', '17:00 | Lamb of God'],
+            emsBands2: ['24:00 | Deadmau5', '22:00 | Tïesto', '20:00 | Hardwell', '19:00 | Dash Berlin', '18:00 | Fedde Legrand', '17:00 | Audien'],
+        }],
+    },
+});
