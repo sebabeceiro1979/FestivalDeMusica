407f83fddf1e6d7fe6f2ef4eeafd62f341c6e28c
Sebastián Beceiro <sebabeceiro@gmail.com>
2022-09-18 16:23:55 -0300

    Second commit: JSON LineUp
    Streamline and speed up the lineup list modification process using JSON and Vue

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # FestivalDeMusica
 This is a 100% practical project where I could practice HTML, JS, npm, Gulp and Sass, and others like Normalize y Plumber
+
+#Initial commit: 
+Original project
+
+#Second commit: JSON LineUp
+Streamline and speed up the lineup list modification process using JSON and Vue
---
+++ js/lineup.js
@@ -0,0 +1,106 @@
+var vueApp = new Vue({
+    el: "#lineup",
+
+    data: {
+        rockDayOne: [{
+            hour: "24:00",
+            band: "Metallica",
+        },
+        {
+            hour: "22:00",
+            band: "Pearl Jam",
+        },
+        {
+            hour: "20:00",
+            band: "KoRn",
+        },
+        {
+            hour: "19:00",
+            band: "Muse",
+        },
+        {
+            hour: "18:00",
+            band: "AC/DC",
+        },
+        {
+            hour: "17:00",
+            band: "Motorhead",
+        },
+        ],
+        electroDayOne: [{
+            hour: "24:00",
+            band: "Deadmou5",
+        },
+        {
+            hour: "22:00",
+            band: "Tiesto",
+        },
+        {
+            hour: "20:00",
+            band: "Hardwell",
+        },
+        {
+            hour: "19:00",
+            band: "Dash Berlin",
+        },
+        {
+            hour: "18:00",
+            band: "Fedde Legrand",
+        },
+        {
+            hour: "17:00",
+            band: "Audien",
+        },
+        ],
+        rockDayTwo: [{
+            hour: "24:00",
+            band: "Ozzy Osbourne",
+        },
+        {
+            hour: "22:00",
+            band: "Slipknot",
+        },
+        {
+            hour: "20:00",
+            band: "Nigthwish",
+        },
+        {
+            hour: "19:00",
+            band: "Evanescence",
+        },
+        {
+            hour: "18:00",
+            band: "Deftones",
+        },
+        {
+            hour: "17:00",
+            band: "Limp Bizkit",
+        },
+        ],
+        electroDayTwo: [{
+            hour: "24:00",
+            band: "Paul Van Dyk",
+        },
+        {
+            hour: "22:00",
+            band: "Armin Van Buuren",
+        },
+        {
+            hour: "20:00",
+            band: "Above & Beyond",
+        },
+        {
+            hour: "19:00",
+            band: "Eric Prdyz",
+        },
+        {
+            hour: "18:00",
+            band: "Ferry Corsten",
+        },
+        {
+            hour: "17:00",
+            band: "Vini Vici",
+        },
+        ]
+    },
+});
